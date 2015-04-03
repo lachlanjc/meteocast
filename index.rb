868c5622347c31245d5439b38325d112fc6ebf3f
Taran <tns12@scasd.org>
2015-04-03 11:40:34 -0400

    Takes zipcodes and stuff

--- index.rb
+++ index.rb
@@ -1,22 +1,24 @@
 require 'sinatra'
 require 'erb'
+require 'yahoo_weatherman'
 
 get '/' do
-  @current = "overcast"
-  @current_temp = 60
-  @tomorrow = "sunny"
-  @tomorrow_temp = 73
-  erb :weather
+  erb :home
 end
 
 get '/help' do
   erb :navigator
 end
 
-get '/weather' do
-  @current = "overcast"
-  @current_temp = 60
-  @tomorrow = "sunny"
-  @tomorrow_temp = 73
+post '/' do
+  zipcode = params[:zipcode]
+  client = Weatherman::Client.new :unit => 'F'
+  response = client.lookup_by_location(zipcode)
+  @current = response.condition['text']
+  @current_temp = response.condition['temp']
+  @tomorrow = response.forecasts.first['text']
+  @tomorrow_temp = response.forecasts.first['high']
+  @city = response.location['city']
+  @region = response.location['region']
   erb :weather
 end
