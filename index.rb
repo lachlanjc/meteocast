cc0ded975c08c7c22ef83137ccf2db34a5c6503c
lachlanjc <hello@lachlanjc.me>
2015-04-05 15:23:50 -0400

    Refactor with Primer

--- index.rb
+++ index.rb
@@ -1,24 +1,22 @@
-require 'sinatra'
-require 'erb'
-require 'yahoo_weatherman'
+require "sinatra"
+require "erb"
+require "yahoo_weatherman"
 
-get '/' do
+get "/" do
   erb :home
 end
 
-post '/' do
-  client = Weatherman::Client.new :unit => 'F'
+post "/" do
+  client = Weatherman::Client.new :unit => "F"
   response = client.lookup_by_location(params[:zipcode])
-  @current = response.condition['text']
-  @current_temp = response.condition['temp']
-  @tomorrow = response.forecasts.first['text']
-  @tomorrow_temp = response.forecasts.first['high']
-  @city = response.location['city']
-  @region = response.location['region']
-  
-  if @region = ""
-    @region = response.location['country']
-  end
-  
+  @current = response.condition["text"]
+  @current_temp = response.condition["temp"]
+  @tomorrow = response.forecasts.first["text"]
+  @tomorrow_temp = response.forecasts.first["high"]
+
+  @city = response.location["city"]
+  @region = response.location["region"]
+  @region = response.location["country"] if @region == ""
+
   erb :weather
 end
